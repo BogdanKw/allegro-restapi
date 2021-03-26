c4460653cc8068d34abe4679d3af8617ed2e760f
BogdanKw <81417293+BogdanKw@users.noreply.github.com>
2021-03-26 16:08:06 +0100

    Update Parent.java

--- src/main/java/pl/allegro/restapi/main/pojo/categories/Parent.java
+++ src/main/java/pl/allegro/restapi/main/pojo/categories/Parent.java
@@ -1,5 +1,6 @@
 package pl.allegro.restapi.main.pojo.categories;
 
+// Nie jest dla mnie jasne przeznaczenie tego Parent. Rozumiem, że to ParentCategory. Ale skoro to jest tylko id, to może ie potrzeba tworzyć tej klasy? Albo stworzyć klasę CategoryId i w klasie Category id byłoby typy CategoryId i parent byłoby również typu CategoryId
 public class Parent {
 
     private String id;

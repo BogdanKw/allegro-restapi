871264086862bf11cf4771b0c56e5d258977e9ba
BogdanKw <81417293+BogdanKw@users.noreply.github.com>
2021-03-26 16:18:45 +0100

    Update Parameters.java

--- src/main/java/pl/allegro/restapi/main/pojo/parameters/Parameters.java
+++ src/main/java/pl/allegro/restapi/main/pojo/parameters/Parameters.java
@@ -2,6 +2,7 @@ package pl.allegro.restapi.main.pojo.parameters;
 
 import java.util.List;
 
+//Taka sama obserwacja jak przy Errors i Categories
 public class Parameters {
 
     private List<Parameter> parameters = null;

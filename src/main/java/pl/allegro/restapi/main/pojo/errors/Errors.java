5e7f9fda3dcc81c2d297a7c5532fb246c5e36119
BogdanKw <81417293+BogdanKw@users.noreply.github.com>
2021-03-26 16:15:52 +0100

    Update Errors.java

--- src/main/java/pl/allegro/restapi/main/pojo/errors/Errors.java
+++ src/main/java/pl/allegro/restapi/main/pojo/errors/Errors.java
@@ -2,6 +2,7 @@ package pl.allegro.restapi.main.pojo.errors;
 
 import java.util.List;
 
+// Tutaj podobnie jak w kategoriach nie wiem gdzie klasa Errors jest używana, ale być może jest niepotrzebna i lepiej używać List<Error>. Jeśli chciałbyś ją usunąć to chyba usunąłbym też cały pakiet errors i klasę Error przeniósł bezpośrednio do pojo. Jeśli ją zostawisz, to nazwę pakietu zmieniłnym na error
 public class Errors {
 
     private List<Error> errors = null;

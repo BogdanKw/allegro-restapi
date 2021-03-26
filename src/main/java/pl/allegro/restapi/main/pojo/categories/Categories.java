4020a87581abe28a47d25f2587f771958a2c9202
BogdanKw <81417293+BogdanKw@users.noreply.github.com>
2021-03-26 16:13:10 +0100

    Update Categories.java

--- src/main/java/pl/allegro/restapi/main/pojo/categories/Categories.java
+++ src/main/java/pl/allegro/restapi/main/pojo/categories/Categories.java
@@ -2,6 +2,8 @@ package pl.allegro.restapi.main.pojo.categories;
 
 import java.util.List;
 
+//Jeśli tutaj nie robisz żadnych operacji na kategoriach i to faktycznie ma być pojo, to nie wiem czy ta klasa jest potrzebna. Czy nie lepiej posługiwać się List<Category>. ALe nie wiem do czego tego używasz.
+// W nazwie pakietu dałbym liczbę pojedyńczą category.
 public class Categories {
 
     private List<Category> categories = null;

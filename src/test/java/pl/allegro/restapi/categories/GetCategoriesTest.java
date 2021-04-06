f4d6e99ee0a8403b50f69296730774788a67ae42
BogdanKw <81417293+BogdanKw@users.noreply.github.com>
2021-04-06 17:13:40 +0200

    Update GetCategoriesTest.java

--- src/test/java/pl/allegro/restapi/categories/GetCategoriesTest.java
+++ src/test/java/pl/allegro/restapi/categories/GetCategoriesTest.java
@@ -11,6 +11,8 @@ import static pl.allegro.restapi.properties.EndpointManager.getEndpointConfig;
 
 public class GetCategoriesTest extends TestBase {
 
+    // Jak na moje to ten test trochę mało sprawdza. Rozumiem, że przypadki testowe sam sobie miałeś wymyślić, tak?
+    // Nie wiemy czy on zwraca coś, co ma schemat zgodny z Categories, czy wszystkie wymagane dane są uzupełnione, czy parent faktycznie istnieje, czy leaf jest true tylko daltych, które nie mają rodzica. Takie testy mi się nasuwają.
     @Test
     public void givenCategoriesWhenGetCategoriesThenReturnListOfAllCategories() {
         Response response = given()
